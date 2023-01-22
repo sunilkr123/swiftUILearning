6e56dcc06be2261e05fef5cb7ccf72d8b4ad8d29
sunilkr123 <sunilkumarsah6@gmail.com>
2023-01-22 11:48:20 +0530

    State Property

---
+++ LoginSwiftUIDemo/LoginSwiftUIDemo/LoginSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftUIDemoApp.swift
+//  LoginSwiftUIDemo
+//
+//  Created by Sunil Kumar on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
